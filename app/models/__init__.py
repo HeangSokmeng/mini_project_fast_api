f2969e94cd3df86c84e56753076f003b8a6c2c1d
sokmeng031504 <mengsokchea031504@gmail.com>
2025-09-04 09:50:04 +0700

    IMP

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
+from app.models.category import Category
+from app.models.product import Product
 from app.models.user.user import User
 from app.models.user.user_role import UserRole
 
-__all__ = ["User", "UserRole"]
+__all__ = ["User", "UserRole", "Category", "Product"]
